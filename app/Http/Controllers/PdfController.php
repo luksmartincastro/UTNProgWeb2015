07328bfea007aee426fcc1a9564dcb1e92f6ce81
luksmartincastro <luksmartincastro@gmail.com>
2016-01-12 22:50:15 -0300

    intento de generar pdf 2

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,10 +3,11 @@
 namespace adminsel\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades;
 
 //use adminsel\Http\Requests;
 use adminsel\Http\Controllers\Controller;
-use PDF;
+//use PDF;
 
 class PdfController extends Controller
 {
@@ -19,13 +20,16 @@ class PdfController extends Controller
     {
         require_once(base_path()."/vendor/dompdf/dompdf/dompdf_config.inc.php"); 
         //-------------
-        $pdf = PDF::loadView('Reportes.ordenReparacion')->setPaper('a4')->setOrientation('landscape');              
+        /*$pdf = PDF::loadView('Reportes.ordenReparacion')->setPaper('a4')->setOrientation('landscape');              
         return $pdf->stream();
 
         /*$view =  \View::make('pdf.invoice', compact('data', 'date', 'invoice'))->render();
         $pdf = \App::make('dompdf.wrapper');
         $pdf->loadHTML($view);
         return $pdf->stream('invoice');*/
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML('<h1>Test</h1>');
+        return $pdf->stream();
     }
 
     
